87a2043e324d13859a7861a748cbca8d22ec291d
Andrew-Ryer <aryer@csumb.edu>
2026-09-09 23:49:52 -0700

    Tuesday 9/8

--- Resource-Collector-Skeleton/Assets/Resource Collector/Scripts/Feedback/Highlightable.cs
+++ Resource-Collector-Skeleton/Assets/Resource Collector/Scripts/Feedback/Highlightable.cs
@@ -8,24 +8,30 @@ using UnityEngine;
 
 public class Highlightable : MonoBehaviour
 {
-    static readonly int SelectionEnabledId = Shader.PropertyToID("_Selection_Enabled");
-
     Renderer[] _targetRenderers;
 
     void Awake()
     {
         // TODO Slice 3.1: cache every child renderer and begin unselected.
+        _targetRenderers = GetComponentsInChildren<Renderer>();
+        
+        SetHighlighted(false);
     }
 
     public void SetHighlighted(bool isHighlighted)
     {
-        // TODO Slice 3.3: forward the requested state to ApplyHighlight. </> end of Slice 3
-    }
-
-    public void SetSelected(bool isSelected) => SetHighlighted(isSelected);
-
-    void ApplyHighlight(bool isHighlighted)
-    {
+        Debug.Log("Trying to Highlight");
+        
         // TODO Slice 3.2: set _Selection_Enabled on each material that supports it.
+        // TODO Slice 3.3: forward the requested state to ApplyHighlight. </> end of Slice 3
+        foreach (var renderer in _targetRenderers)
+            renderer.material.SetFloat("_Highlight_Enabled", isHighlighted ? 1f : 0f);
     }
+
+    // public void SetSelected(bool isSelected) => SetHighlighted(isSelected);
+    //
+    // void ApplyHighlight(bool isHighlighted)
+    // {
+    //     //  Slice 3.2: set _Selection_Enabled on each material that supports it.
+    // }
 }
